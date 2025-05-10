7d648959b8bc437f0d647464c3175cd8f8159aa5
ishida-san <najmi0612@gmail.com>
2025-05-11 03:12:23 +0800

    Navbar adjustments
    Included hamburger, still not pretty though

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,41 @@
-// src/components/Navbar.js
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import './Navbar.css';
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <div className='navbar'>
-      <h1>KPI Management System</h1>
-      <div className="nav-links">
-        {/* Use Link to navigate between pages */}
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/login">Log In</Link>
-        <Link to="/signup">Sign Up</Link>
-        <Link to="/manager">Manager</Link>
+    <>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <Link to="/" className="navbar-brand">
+            <h1>KPIHub</h1>
+          </Link>
+          
+          <button 
+            className={`hamburger ${isOpen ? 'open' : ''}`}
+            onClick={() => setIsOpen(!isOpen)}
+            aria-label="Menu"
+          >
+            <span></span>
+            <span></span>
+            <span></span>
+          </button>
+          
+          <div className="nav-links desktop-links">
+            <Link to="/login">Log In</Link>
+            <Link to="/signup">Sign Up</Link>
+          </div>
+        </div>
+      </nav>
+      
+      {/* Mobile menu (separate element) */}
+      <div className={`mobile-menu ${isOpen ? 'open' : ''}`}>
+        <Link to="/login" onClick={() => setIsOpen(false)}>Log In</Link>
+        <Link to="/signup" onClick={() => setIsOpen(false)}>Sign Up</Link>
       </div>
-    </div>
+    </>
   );
 };
 
