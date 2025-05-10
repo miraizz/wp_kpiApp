d8092ec5e642a0df19c8b4017a196b7792efa2d5
ishida-san <najmi0612@gmail.com>
2025-05-11 00:44:28 +0800

    Update

---
+++ src/pages/Manager.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import './Manager.css';
+
+const Manager = () => {
+  return (
+    <div className="manager-page">
+      <div className="content-container">
+        <h2 className="heading">Manager Dashboard</h2>
+        <p className="description">Manage and track key performance indicators</p>
+      </div>
+      
+      <div className="cards-container">
+        <div className="card">
+          <div className="card-title total-kpis">4</div>
+          <div className="card-content">Total</div>
+        </div>
+
+        <div className="card">
+          <div className="card-title on-track">2</div>
+          <div className="card-content">On Track</div>
+        </div>
+
+        <div className="card">
+          <div className="card-title at-risk">3</div>
+          <div className="card-content">At Risk</div>
+        </div>
+
+        <div className="card">
+          <div className="card-title behind">4</div>
+          <div className="card-content">Behind</div>
+        </div>
+      </div>
+
+      <div className='big-card-container'>
+        <div className='big-card'>
+          
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Manager;
